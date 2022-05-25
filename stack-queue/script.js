0fc8c1cc86afead40fa38457873206abd6e17b9a
luigimoraes3@gmail.com <luigimoraes3@gmail.com>
2022-05-25 13:27:52 -0300

    add: css overflow/underflow styling

--- stack-queue/script.js
+++ stack-queue/script.js
@@ -3,7 +3,9 @@ import { Stack, Queue } from './data-structures.js';
 const stackUl = document.getElementById("stack"),
       stackSize = document.getElementById("stackSize"),
       queueUl = document.getElementById("queue"),
-      queueSize = document.getElementById("queueSize");
+      queueSize = document.getElementById("queueSize"),
+      stackWarn = document.getElementById("stackWarn"),
+      queueWarn = document.getElementById("queueWarn");
 
 const pushStackBtn = document.getElementById("pushStack"),
       popStackBtn = document.getElementById("popStack"),
@@ -56,41 +58,55 @@ function removeULChild(ds){
 pushStackBtn.addEventListener("click", (e) => {
   if(stack.isFull()){
     console.warn("Overflow");
+    stackWarn.style.visibility = "visible";
+    stackWarn.textContent = "Overflow!"
   }else{
     stack.push();
     newULChild(stack.peek(), "stack");
     stackSize.textContent = stack.size;
+    stackWarn.textContent = "";
+    stackWarn.style.visibility = "hidden";
   }
 });
 
 popStackBtn.addEventListener("click", (e) => {
   if(stack.isEmpty()){
     console.warn("Underflow");
+    stackWarn.style.visibility = "visible";
+    stackWarn.textContent = "Underflow!"
   }else{
     const popped = stack.pop();
     removeULChild("stack");
     console.warn("Saiu da pilha: "+popped);
     stackSize.textContent = stack.size;
+    stackWarn.textContent = "";
+    stackWarn.style.visibility = "hidden";
   }
 });
 
 enqueueBtn.addEventListener("click", (e) => {
   if(queue.isFull()){
     console.warn("Overflow");
+    queueWarn.style.visibility = "visible";
+    queueWarn.textContent = "Overflow!";
   }else{
     queue.enqueue();
     newULChild(queue.peek(), "queue");
     queueSize.textContent = queue.size;
+    queueWarn.style.visibility = "hidden";
   }
 });
 
 dequeueBtn.addEventListener("click", (e) => {
   if(queue.isEmpty()){
     console.warn("Underflow");
+    queueWarn.style.visibility = "visible";
+    queueWarn.textContent = "Underflow";
   }else{
     const dequeued = queue.dequeue();
     removeULChild("queue");
     console.warn("Saiu da fila: " + dequeued);
     queueSize.textContent = queue.size;
+    queueWarn.style.visibility = "hidden";
   }
 });
